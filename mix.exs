b9e4e3f5b7b052cbe0e74fa611e9f5516d4ba6df
Robert Brown <ammoknight@gmail.com>
2020-11-28 15:44:04 -0700

    Update Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Genetic.MixProject do
     [
       app: :genetic,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
